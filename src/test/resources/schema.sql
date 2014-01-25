f5f2c6db4b27eb622fefb77d65f673dfb351f20e
Rafizan Baharum <rafizan.baharum@gmail.com>
2014-01-26 02:51:42 +0800

    Added datasource + schema

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,6 +1,6 @@
-CREATE USER kpn_media WITH PASSWORD 'kpn_media';
-CREATE DATABASE kpn_media;
-GRANT ALL PRIVILEGES ON DATABASE kpn_media to kpn_media
+CREATE USER media WITH PASSWORD 'media';
+CREATE DATABASE media;
+GRANT ALL PRIVILEGES ON DATABASE media to media
 ;
 
 
