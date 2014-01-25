0a6802475c98acdf710245488d07b8cb8f5c3952
Rafizan Baharum <rafizan.baharum@gmail.com>
2014-01-26 02:43:56 +0800

    Added manager

---
+++ src/main/java/my/gov/kpn/media/biz/manager/DirectoryManager.java
@@ -0,0 +1,49 @@
+package my.gov.kpn.media.biz.manager;
+
+
+import my.gov.kpn.media.core.model.KpnDirectory;
+import my.gov.kpn.media.core.model.KpnMedia;
+
+import java.util.List;
+
+/**
+ * @author rafizan.baharum
+ * @since 1/13/14
+ */
+public interface DirectoryManager {
+
+    KpnDirectory findDirectoryById(Long id);
+
+    KpnDirectory findDirectoryByName(String name);
+
+    List<KpnDirectory> findDirectories();
+
+    List<KpnDirectory> findDirectories(Integer offset, Integer limit);
+
+    List<KpnMedia> findMedias(Integer offset, Integer limit);
+
+    List<KpnMedia> findMedias(KpnDirectory directory, Integer offset, Integer limit);
+
+    Integer countDirectory();
+
+    Integer countMedia();
+
+    Integer countMedia(KpnDirectory directory);
+
+    boolean isDirectoryExists(String name);
+
+    boolean isMediaExists(String name);
+
+    void saveDirectory(KpnDirectory directory);
+
+    void updateDirectory(KpnDirectory directory);
+
+    void removeDirectory(KpnDirectory directory);
+
+    void addMedia(KpnDirectory directory, KpnMedia media);
+
+    void updateMedia(KpnDirectory directory, KpnMedia media);
+
+    void removeMedia(KpnDirectory directory, KpnMedia media);
+
+}
---
+++ src/main/java/my/gov/kpn/media/biz/manager/impl/DirectoryManagerImpl.java
@@ -0,0 +1,129 @@
+package my.gov.kpn.media.biz.manager.impl;
+
+import my.gov.kpn.media.biz.manager.DirectoryManager;
+import my.gov.kpn.media.core.dao.KpnDirectoryDao;
+import my.gov.kpn.media.core.dao.KpnMediaDao;
+import my.gov.kpn.media.core.model.KpnDirectory;
+import my.gov.kpn.media.core.model.KpnMedia;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @author rafizan.baharum
+ * @since 1/13/14
+ */
+@Service("directoryManager")
+@Transactional
+public class DirectoryManagerImpl implements DirectoryManager {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private KpnDirectoryDao directoryDao;
+
+    @Autowired
+    private KpnMediaDao mediaDao;
+
+    @Override
+    public KpnDirectory findDirectoryById(Long id) {
+        return directoryDao.findById(id);
+    }
+
+    @Override
+    public KpnDirectory findDirectoryByName(String name) {
+        return directoryDao.findByName(name);
+    }
+
+    @Override
+    public List<KpnDirectory> findDirectories() {
+        return directoryDao.find(0, 99999);
+    }
+
+    @Override
+    public List<KpnDirectory> findDirectories(Integer offset, Integer limit) {
+        return directoryDao.find(offset, limit);
+    }
+
+    @Override
+    public List<KpnMedia> findMedias(Integer offset, Integer limit) {
+        return mediaDao.find(offset, limit);
+    }
+
+    @Override
+    public List<KpnMedia> findMedias(KpnDirectory directory, Integer offset, Integer limit) {
+        return mediaDao.find(directory, offset, limit);
+    }
+
+    @Override
+    public Integer countDirectory() {
+        return directoryDao.count();
+
+    }
+
+    @Override
+    public Integer countMedia() {
+        return mediaDao.count();
+    }
+
+    @Override
+    public Integer countMedia(KpnDirectory directory) {
+        return mediaDao.count(directory);
+    }
+
+    @Override
+    public boolean isDirectoryExists(String name) {
+        return false;  // TODO:
+
+    }
+
+    @Override
+    public boolean isMediaExists(String name) {
+        return false;  // TODO:
+
+    }
+
+    @Override
+    public void saveDirectory(KpnDirectory directory) {
+        // TODO:
+
+    }
+
+    @Override
+    public void updateDirectory(KpnDirectory directory) {
+        // TODO:
+
+    }
+
+    @Override
+    public void removeDirectory(KpnDirectory directory) {
+        // TODO:
+
+    }
+
+    @Override
+    public void addMedia(KpnDirectory directory, KpnMedia media) {
+        // TODO:
+
+    }
+
+    @Override
+    public void updateMedia(KpnDirectory directory, KpnMedia media) {
+        // TODO:
+
+    }
+
+    @Override
+    public void removeMedia(KpnDirectory directory, KpnMedia media) {
+        // TODO:
+
+    }
+}
